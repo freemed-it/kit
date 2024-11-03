9097f98ae6332b2b136cca197323f6cd3be77998
Jaei Kim <98146849+yeeezae@users.noreply.github.com>
2024-11-03 13:03:26 +0900

    feat(ui): Badge 컴포넌트 구현 (#18)
    * feat: Badge 컴포넌트 구현
    
    * docs: Badge stories 작성
    
    * docs: add changeset
    
    * feat: variant 추가

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -2,3 +2,4 @@ import './styles/globals.css'
 
 export { Button } from './components/button'
 export { Avatar, AvatarImage, AvatarFallback } from './components/avatar'
+export { Badge } from './components/badge'

251c10cd95b9a7e882bec4b0140a3887b4a45ba1
Jaei Kim <98146849+yeeezae@users.noreply.github.com>
2024-11-10 10:11:55 +0900

    feat(ui): Textarea 컴포넌트 구현 (#20)
    * docs: Textarea stories 작성
    
    * docs: add changeset
    
    * feat: Textarea 컴포넌트 구현
    
    * feat: render 수정
    
    * comment: 주석 삭제

---
+++ apps/docs/stories/textarea.stories.tsx
@@ -0,0 +1,62 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Button, Label, Textarea } from '@freemed-kit/ui'
+
+const meta: Meta<typeof Textarea> = {
+  title: 'Components/Textarea',
+  component: Textarea,
+  tags: ['autodocs'],
+  argTypes: {
+    placeholder: { control: 'text', defaultValue: 'Enter your text here...' },
+    disabled: { control: 'boolean' },
+  },
+}
+
+export default meta
+type Story = StoryObj<typeof Textarea>
+
+export const Default: Story = {
+  args: {
+    placeholder: 'Enter your text here...',
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    placeholder: 'Disabled Textarea',
+    disabled: true,
+  },
+}
+
+export const WithContent: Story = {
+  args: {
+    children: 'This is pre-filled content in the textarea',
+  },
+}
+
+export const WithLabel: Story = {
+  render: args => (
+    <div className="tw-grid tw-w-full tw-gap-1.5">
+      <Label htmlFor="message">Your message</Label>
+      <Textarea id="message" placeholder="Type your message here." />
+    </div>
+  ),
+}
+
+export const WithText: Story = {
+  render: args => (
+    <div className="tw-grid tw-w-full tw-gap-1.5">
+      <Label htmlFor="message-2">Your Message</Label>
+      <Textarea id="message-2" placeholder="Type your message here." />
+      <p className="tw-text-sm tw-text-gray-300">Your message will be copied to the support team.</p>
+    </div>
+  ),
+}
+
+export const WithButton: Story = {
+  render: args => (
+    <div className="tw-grid tw-w-full tw-gap-2">
+      <Textarea placeholder="Type your message here." />
+      <Button>Send message</Button>
+    </div>
+  ),
+}

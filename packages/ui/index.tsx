fe3f6c44f10927fe17590f1958e48e6ee4dba259
Seunghee Lee <49032882+eeseung@users.noreply.github.com>
2024-11-10 09:38:32 +0900

    feat(ui): Table 컴포넌트 구현 (#19)
    * feat: Table 컴포넌트 구현
    
    * docs: Table stories 작성
    
    * docs: add changeset

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -3,3 +3,13 @@ import './styles/globals.css'
 export { Button } from './components/button'
 export { Avatar, AvatarImage, AvatarFallback } from './components/avatar'
 export { Badge } from './components/badge'
+export {
+  Table,
+  TableHeader,
+  TableBody,
+  TableFooter,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableCaption,
+} from './components/table'

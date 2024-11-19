d2badf2984d01e66984fadc01004d32c48ceb2aa
kjjlss2 <jgim0417@gmail.com>
2024-11-19 20:47:44 +0900

    docs: Dialog stories 수정

--- apps/docs/stories/dialog.stories.tsx
+++ apps/docs/stories/dialog.stories.tsx
@@ -31,7 +31,7 @@ export const Default: Story = {
       <DialogContent className="tw-sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>Make changes to your profile here. Click save</DialogDescription>
+          <DialogDescription>Make changes to your profile here. Click save when you&apos;re done.</DialogDescription>
         </DialogHeader>
         <div className="tw-grid tw-gap-4 tw-py-4">
           <div className="tw-grid tw-grid-cols-4 tw-items-center tw-gap-4">
@@ -56,20 +56,31 @@ export const DialogWithButton: Story = {
   render: args => (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline">Share</Button>
+        <Button variant="outline">Edit Profile</Button>
       </DialogTrigger>
-      <DialogContent className="tw-sm:max-w-md">
+      <DialogContent className="tw-sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Share link</DialogTitle>
-          <DialogDescription>Anyone who has this link will be able to view this.</DialogDescription>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>Make changes to your profile here. Click save when you&apos;re done.</DialogDescription>
         </DialogHeader>
-        <DialogFooter className="tw-sm:justify-start">
-          <DialogClose>
-            <Button className="tw-mr-2.5" type="button" variant="outline">
-              취소
-            </Button>
+        <div className="tw-grid tw-gap-4 tw-py-4">
+          <div className="tw-grid tw-grid-cols-4 tw-items-center tw-gap-4">
+            <Label className="tw-text-right" htmlFor="name">
+              Name
+            </Label>
+            <Input className="tw-col-span-3" defaultValue="Pedro Duarte" id="name" />
+          </div>
+          <div className="tw-grid tw-grid-cols-4 tw-items-center tw-gap-4">
+            <Label className="tw-text-right" htmlFor="username">
+              Username
+            </Label>
+            <Input className="tw-col-span-3" defaultValue="@peduarte" id="username" />
+          </div>
+        </div>
+        <DialogFooter className="sm:justify-start">
+          <DialogClose asChild>
             <Button type="button" variant="default">
-              확인
+              Close
             </Button>
           </DialogClose>
         </DialogFooter>

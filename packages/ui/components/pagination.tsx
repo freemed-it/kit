5d1e004d81c8aad1369ae5d31274a0b902bbab38
eeseung <eeseungh@gmail.com>
2024-11-21 12:42:28 +0900

    feat(ui): Pagination 컴포넌트 이전/다음 텍스트 변경

--- packages/ui/components/pagination.tsx
+++ packages/ui/components/pagination.tsx
@@ -49,14 +49,14 @@ PaginationLink.displayName = 'PaginationLink'
 const PaginationPrevious = ({ className, ...props }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink aria-label="Go to previous page" size="default" className={cn('gap-1 pl-2.5', className)} {...props}>
     <ChevronLeft className="size-4" />
-    <span>Previous</span>
+    <span>이전</span>
   </PaginationLink>
 )
 PaginationPrevious.displayName = 'PaginationPrevious'
 
 const PaginationNext = ({ className, ...props }: React.ComponentProps<typeof PaginationLink>) => (
   <PaginationLink aria-label="Go to next page" size="default" className={cn('gap-1 pr-2.5', className)} {...props}>
-    <span>Next</span>
+    <span>다음</span>
     <ChevronRight className="size-4" />
   </PaginationLink>
 )

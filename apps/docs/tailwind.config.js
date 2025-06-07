dd36a8c95babf637001e8580712af66d7aca2285
eeseung <eeseungh@gmail.com>
2025-06-07 16:07:04 +0900

    feat(docs): upgrade tailwindcss to v4

--- apps/docs/tailwind.config.js
+++
@@ -1,7 +0,0 @@
-const baseConfig = require('@freemed-kit/ui/tailwind.config')
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  ...baseConfig,
-  content: ['./stories/**/*.{js,jsx,ts,tsx}'],
-}
